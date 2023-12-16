cf2f6370f39b1a0a70329a05ad1a89c0837c8dcf
Dum60 <d.siomov34@gmail.com>
2023-12-16 14:17:08 +0300

    feat: scripts

---
+++ scripts/navActive.js
@@ -0,0 +1,16 @@
+(() => {
+    window.addEventListener('load', () => {
+        const loc = window.location.pathname.split('/').at(-1);
+
+        let menuItems = document.querySelectorAll('.nav-list > li > a');
+        console.log(menuItems)
+
+        menuItems.forEach(function (item) {
+            if (item.getAttribute('href') === loc) {
+                const parent = item.parentNode
+                parent.classList.add('nav-active')
+                parent.classList.remove('tile')
+            }
+        })
+    })
+})()
